2562fbf9b972bf528748b226ce0fd08de2e406e1
James Charnock <jamescharnock@live.co.uk>
2021-01-02 15:21:27 +0000

    Finished BTCExRateAPI interpret response

--- src/main.java
+++ src/main.java
@@ -3,10 +3,9 @@ import java.util.Arrays;
 
 public class main {
         public static void main(String[] args) throws IOException {
-                String response = BTCExRateAPI.readPage();
-                String myString = "check (this is the string) this (shouldn't be contained)";
-                System.out.println(BTCExRateAPI.getContents(myString, "("));
-                String[] interpreted = BTCExRateAPI.interpretResponse(response);
-                System.out.println(Arrays.toString(interpreted));
+                new BTCExRateAPI();
+                System.out.println(BTCExRateAPI.getPriceGBP());
+                System.out.println(BTCExRateAPI.getPriceUSD());
+                System.out.println(BTCExRateAPI.getDateTime());
         }
 }
